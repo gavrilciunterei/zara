0543eb2ec542e209f506e56b6c7438990cb8b6b5
gavrilciunterei <gavriltaller@gmail.com>
2022-12-15 22:12:06 +0100

    feat: background app

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import Home from "./pages/home";
 import Chapter from "./pages/chapter";
 import Podcast from "./pages/podcast";
 import { store } from "./store/rootStore";
-
+import Background from "./components/Background";
 function App() {
   return (
     <BrowserRouter>
-      <Provider store={store}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/chapter" element={<Chapter />} />
-          <Route path="/podcast" element={<Podcast />} />
-        </Routes>
-      </Provider>
+      <Background>
+        <Provider store={store}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/chapter" element={<Chapter />} />
+            <Route path="/podcast" element={<Podcast />} />
+          </Routes>
+        </Provider>
+      </Background>
     </BrowserRouter>
   );
 }
---
+++ src/components/Background/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Background = ({children}) => {
+  return (
+    <div className="flex mx-20 my-20">{children}</div>
+  )
+}
+
+export default Background
