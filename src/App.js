968ae5ab0ab72295252058bc1233aebdb55a7b67
gavrilciunterei <gavriltaller@gmail.com>
2022-12-15 01:30:29 +0100

    feat: installed route and creaded pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+import Chapter from "./pages/chapter";
+import Podcast from "./pages/podcast";
+
 function App() {
-  return <div>Hola!</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/chapter" element={<Chapter />} />
+        <Route path="/podcast" element={<Podcast />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
