d0234509d43937aee561822ff25f32ae9baa04c9
gavrilciunterei <gavriltaller@gmail.com>
2022-12-16 22:43:16 +0100

    fix: update cors link and get by id query

--- src/store/podcast/index.js
+++ src/store/podcast/index.js
@@ -3,14 +3,15 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const createPodcastApi = createApi({
   reducerPath: "podcast",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://api.allorigins.win/raw?url=https://itunes.apple.com",
+    baseUrl: "https://cors-anywhere.herokuapp.com/https://itunes.apple.com",
   }),
   endpoints: (builder) => ({
     getPodcasts: builder.query({
       query: () => "/us/rss/toppodcasts/limit=100/genre=1310/json",
     }),
     getPodcastsById: builder.query({
-      query: (id) => `/lookup?id=${id}`,
+      query: (id) =>
+        `/lookup?id=${id}&media=podcast&entity=podcastEpisode&limit=100`,
     }),
   }),
   refetchOnMountOrArgChange: 86400,
