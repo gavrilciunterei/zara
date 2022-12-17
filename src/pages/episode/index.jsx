a7a739acc20cbd859d8e7aad29fd5dd2ba91eb3f
gavrilciunterei <gavriltaller@gmail.com>
2022-12-17 19:25:03 +0100

    feat: parse html

--- src/pages/episode/index.jsx
+++ src/pages/episode/index.jsx
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom";
 import LeftTab from "../../components/LeftTab";
 import Spinner from "../../components/Spinner";
 import useFetchEpisode from "./hooks/useFetchEpisode";
+import parse from "html-react-parser";
 
 const Episode = () => {
   const { episodeId, podcastId } = useParams();
@@ -16,7 +17,9 @@ const Episode = () => {
       {isFetchingEpisode && <Spinner />}
       <div className="bg-white shadow-lg p-4 max-w-2xl">
         <p className="font-bold mb-4">{episode.episodeTitle}</p>
-        <p className="font-light italic mb-10">{episode.episodeDescription}</p>
+        <p className="font-light italic mb-10">
+          {parse(episode.episodeDescription)}
+        </p>
         <audio src={episode.episodeUrl} controls className="w-full" />
       </div>
     </div>
