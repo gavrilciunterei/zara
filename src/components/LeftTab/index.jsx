fa4a2b5637ae3cd99ebb9b6ae363ddea33cc6708
gavrilciunterei <gavriltaller@gmail.com>
2022-12-17 19:06:54 +0100

    feat: can click on image and title podcast

--- src/components/LeftTab/index.jsx
+++ src/components/LeftTab/index.jsx
@@ -1,22 +1,38 @@
 import useFetchPodcast from "./hooks/useFetchPodcast";
 import Spinner from "../Spinner";
+import { useNavigate, useParams } from "react-router-dom";
 
 const LeftTab = ({ podcastId }) => {
   const { podcast, isFetchingPodcast } = useFetchPodcast({ id: podcastId });
+  const { episodeId } = useParams();
+  const navigate = useNavigate();
+
+  const handleOnClick = () => {
+    if (!episodeId) return;
+    navigate(`/podcast/${podcastId}`);
+  };
 
   return isFetchingPodcast ? (
     <Spinner />
   ) : (
     <div className="bg-white shadow-lg flex mr-20 p-4 flex-col max-w-xs h-max rounded-sm">
-      <div className="flex  justify-center">
+      <div className="flex justify-center">
         <img
+          onClick={handleOnClick}
           src={podcast.image}
           alt={podcast.title}
-          className="w-40 h-40 shadow-md rounded-lg"
+          className={`w-40 h-40 shadow-md rounded-lg ${
+            episodeId && "cursor-pointer"
+          }`}
         />
       </div>
       <hr className="h-px border-0 dark:bg-gray-200 mt-2 mb-4" />
-      <p className="font-bold">{podcast.title}</p>
+      <p
+        className={`font-bold ${episodeId && "cursor-pointer"}`}
+        onClick={handleOnClick}
+      >
+        {podcast.title}
+      </p>
       <p className="text-gray-500">{podcast.artist}</p>
       <hr className="h-px border-0 dark:bg-gray-200 mt-2 mb-4" />
       <p className="font-bold">Description:</p>
