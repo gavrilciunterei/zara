72b02839455614090a1fa5a76a1acdfc3d9d509c
gavrilciunterei <gavriltaller@gmail.com>
2022-12-17 20:27:18 +0100

    feat: text size

--- src/components/Logo/index.jsx
+++ src/components/Logo/index.jsx
@@ -11,7 +11,7 @@ const Logo = () => {
     <div>
       <div className="flex flex-row  content-end justify-between items-end">
         <button onClick={() => handleClick()} className="">
-          <p className="text-blue-500 font-bold">Podcaster</p>
+          <p className="text-blue-500 font-bold text-2xl">Podcaster</p>
         </button>
         <PulsatingCircle />
       </div>
